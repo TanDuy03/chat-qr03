40e7607d6c7fe1374db0c59aad1f889f1a652021
TanDuy03 <ng.tanduy261203@gmail.com>
2024-05-27 21:55:05 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,11 @@ import PlausibleProvider from 'next-plausible';
 
 const inter = Inter({ subsets: ['latin'] });
 
-let title = 'QrGPT - QR Code Generator';
+let title = 'Nguyen Tan Duy - Software Developer';
 let description = 'Generate your AI QR Code in seconds';
-let url = 'https://www.qrgpt.io';
-let ogimage = 'https://www.qrgpt.io/og-image.png';
-let sitename = 'qrGPT.io';
+let url = '';
+let ogimage = '';
+let sitename = '';
 
 export const metadata: Metadata = {
   metadataBase: new URL(url),
